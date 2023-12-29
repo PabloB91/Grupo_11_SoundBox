06c96aa46cdc7fe114e97a8417ec8ab68eec4e81
Miguel-A-VN <120611559+Miguel-A-VN@users.noreply.github.com>
2023-12-29 17:08:56 -0500

    estructurando el entry poind

--- src/app.js
+++ src/app.js
@@ -1,33 +1,57 @@
+//*****************************************************************************************************\\
+
 const express = require("express");
 const path = require("path")
 const app = express();
 const fs = require("fs");
-// const methodOverride = require('method-override'); // Para poder usar los métodos PUT y DELETE
+const methodOverride = require('method-override'); // requiriendo method para usar put y delate
 // const logMiddleware = require("./middlewares/logMiddleware");
 
-app.use(express.static("public"));
+//*****************************************************************************************************\\
 
-// Template engine
+app.use(express.static("public")); // para usar los archivos estaticos de la carpeta public
+
+//*****************************************************************************************************\\
+
+/************* Template engine (ejs) *************/
 
 app.set("view engine", "ejs")
-
 app.set('views', path.resolve(__dirname, "views"));
-// Rutas
 
-// Ruta de /products
-const productsRouter = require('./routes/productsRouter'); 
+//*****************************************************************************************************\\
 
+// ** Rutas **
+
+// index
 const mainRouter = require("./routes/mainRouter");
 
-app.use("/", mainRouter);
+// products
+const productsRouter = require('./routes/productsRouter'); 
 
-app.listen(3050,()=> {
-    console.log ("Servidor funcionando en: http://localhost:3050/")
-});
+app.use("/", mainRouter);
+app.use("/products", productsRouter);
 
 app.use((req, res, next) => {
     res.status(404).render("not-found")
 });
 
-// Middlewares
+//*****************************************************************************************************\\
+
+/************* Middlewares *************/
+
+// Para tomar los datos del body
+app.use(express.urlencoded({extended: false}));
+app.use(express.json) 
+
+// Para poder usar los metodos put y delete
+app.use(methodOverride('_method'));
 // app.use(logMiddleware);
+
+
+//*****************************************************************************************************\\
+
+/************* LLAMANDO AL SERVIDOR *************/
+app.listen(3050,()=> {
+    console.log ("Servidor funcionando en: http://localhost:3050/")
+});
+//*****************************************************************************************************\\
