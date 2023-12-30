5c9763a6a631b6db2ccee908d98dff1cb4d75d7f
Miguel-A-VN <120611559+Miguel-A-VN@users.noreply.github.com>
2023-12-30 11:55:11 -0500

    arganisando la sintaxis y error de que no cargaba ere porque me faltaba poner el express.json() con los parentesis quedo funcando para resibir el objeto literal pero aun sigue en proceso el crear producto

--- src/app.js
+++ src/app.js
@@ -2,18 +2,33 @@
 
 const express = require("express");
 const path = require("path")
-const app = express();
 const fs = require("fs");
 const methodOverride = require('method-override'); // requiriendo method para usar put y delate
 // const logMiddleware = require("./middlewares/logMiddleware");
 
+const app = express();
+
 //*****************************************************************************************************\\
 
+/************* Middlewares *************/
 app.use(express.static("public")); // para usar los archivos estaticos de la carpeta public
 
+// Para tomar los datos del body
+app.use(express.urlencoded({extended: false}));
+app.use(express.json()) 
+
+// Para poder usar los metodos put y delete
+app.use(methodOverride('_method'));
+// app.use(logMiddleware);
 
 //*****************************************************************************************************\\
 
+/************* Template engine (ejs) *************/
+
+app.set("view engine", "ejs")
+app.set('views', path.resolve(__dirname, "views"));
+
+//*****************************************************************************************************\\
 // ** Rutas **
 
 // index
@@ -29,25 +44,6 @@ app.use("/products", productsRouter);
 app.use((req, res, next) => {
     res.status(404).render("not-found")
 });
-
-//*****************************************************************************************************\\
-
-/************* Middlewares *************/
-
-// Para tomar los datos del body
-app.use(express.urlencoded({extended: false}));
-app.use(express.json) 
-
-// Para poder usar los metodos put y delete
-app.use(methodOverride('_method'));
-// app.use(logMiddleware);
-
-//*****************************************************************************************************\\
-
-/************* Template engine (ejs) *************/
-
-app.set("view engine", "ejs")
-app.set('views', path.resolve(__dirname, "views"));
 
 //*****************************************************************************************************\\
 
