b1860ca88d55bbeb6be4cc601504364f278928c4
user.email <juan.fosch@gmail.com>
2024-01-17 15:59:08 -0300

    Crear Producto listo y arreglos estéticos  en Listado

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -25,13 +25,12 @@ const controller = {
 		let productoDefinido = products.find(producto => {
 			return producto.id == idProducto
 		})
-				if(productoDefinido){
-					res.render("productDetail", { singleProduct : productoDefinido })
-				} else{
-					res.send("ERROR")
-				}
-	
 
+		if(productoDefinido){
+			res.render("productDetail", { singleProduct : productoDefinido })
+		} else{
+			res.send("ERROR")
+		}
 	},
 
 	
@@ -47,34 +46,40 @@ const controller = {
 
 	// (post) Create - Método para guardar la info
 	processCreate: (req, res) => {
-
+		//console.log(req.file.filename);
 		//Gusrdar el producto con la informacion del usuario
-
+		
 		const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+		/* console.log(req.files[0]["filename"]);
+		console.log(req.files[1]); */
+		console.log(req.body.coloresDisponibes);
+
+		
 
 		const newProduct = {
 			id: products[products.length - 1].id + 1,
-			imagen: req.files[0]["filename"],
-			imagenFrontal: req.files[1]["filename"],
-			imagenLateralDerecha: req.files[2]["filename"],
-			imagenLateralIzquierda: req.files[3]["filename"],
-			nombre: req.body.nombre,
-			marca: req.body.marca,
-			precio: req.body.precio,
-			descuento: req.body.descuento,
-			descripcion: req.body.descripcion,
-			cantidad: req.body.cantidad,
-			coloresDisponibes: req.body.coloresDisponibes,
-			categorias: req.body.categorias
+			imagen: req.files[0] == undefined ? "": req.files[0]["filename"],
+			imagenFrontal: req.files[1] == undefined ? "": req.files[1]["filename"],
+			imagenLateralDerecha: req.files[2] == undefined ? "": req.files[2]["filename"],
+			imagenLateralIzquierda: req.files[3]== undefined ? "": req.files[3]["filename"],   
+			marca: req.body.marca == undefined ? "": req.body.marca,
+			nombre: req.body.nombre == undefined ? "": req.body.nombre,
+			precio: req.body.precio == undefined ? "": parseInt(req.body.precio),
+			descuento: req.body.descuento == undefined ? "": req.body.descuento,
+			descripcion: req.body.descripcion == undefined ? "": req.body.descripcion,
+			cantidad: req.body.cantidad == undefined ? "": parseInt(req.body.cantidad),
+			coloresDisponibles: req.body.coloresDisponibles == undefined ? [""]: [req.body.coloresDisponibles],
+			categorias: req.body.categorias == undefined ? "": [req.body.categorias],
+			estado: req.body.estado == undefined ? "": req.body.estado
 		}
-/
+
 		products.push(newProduct);
 
 		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '));
 
 		// mostrar lo que se guardo en una vista
 
-		res.redirect('/admin')
+		res.redirect('/todosLosProductos')
 	},
 
 	// (get) Update - Formulario para editar
