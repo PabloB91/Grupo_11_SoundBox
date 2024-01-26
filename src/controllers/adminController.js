121a30f68f396d1145cb1d548a388e94029d0451
Miguel-A-VN <120611559+Miguel-A-VN@users.noreply.github.com>
2024-01-26 12:33:09 -0500

    volviend dinamica la vista de listUsers faltan estilos y seponsive

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const path = require("path")
+const fs = require('fs')
+
+const usersFilePath = path.join(__dirname, '../data/users.json')
 
 const aController = {
-
+    
     listUsers :(req, res) => {
-        res.render("listUsers.ejs")
+
+        const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+        res.render("listUsers.ejs", {users})
     }
 }
 module.exports = aController;
