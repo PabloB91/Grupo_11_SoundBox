3ab078361bf4b2dba706c6293fe4c77aa22ed44f
FranCapi2005 <ftcapitani@gmail.com>
2024-01-27 17:02:13 -0300

    controller & routes edit users

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,3 +1,5 @@
+// Requerimientos 
+
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
@@ -5,30 +7,41 @@ const path = require("path");
 
 const usersController = require('../controllers/usersController');
 const { body } = require('express-validator');
-// validaciones
-const validacionesRegistro= [
+
+
+
+// Validacion de Registro
+
+const registerValidations = [
     body('name').notEmpty().withMessage('Tu nombre es necesario'),
     body('lastName').notEmpty().withMessage('Tu apellido es necesario'),
-    body('email').trim().not().isEmpty().withMessage('Tu E-mail es necesario'),
+    body('email').trim().notEmpty().isEmpty().withMessage('Tu E-mail es necesario'),
     body('email').isEmail().withMessage('Ingresa una dirección valida'),
     body('password').notEmpty().isLength({min: 8, max: 16 }).withMessage('La contraseña debe tener entre 8 y 16 caracteres'),
     body('password').matches(/[A-Z]/).withMessage('La contraseña debe tener al menos una mayúscula'),
     body('password').matches(/[a-z]/).withMessage('La contraseña debe tener al menos una minúscula'),
     body('password').matches(/[!@#$%^&*]/).withMessage('La contraseña debe tener al menos un carácter especial (!@#$%^&*)'),
-    body('confirm-password').custom((validacionDePassword, { req }) => {
-        if (validacionDePassword !== req.body.password) {
+    body('confirm-password').custom((validationPassword, { req }) => {
+        if (validationPassword !== req.body.password) {
           throw new Error('La contraseña no es la misma que se ingresó en el campo anterior');
         }
         return true;
       }),
 ];
 
-const valLogin = [
-    body('password').notEmpty().withMessage('Ingresar contrasena'),
-    body('email').isEmail().withMessage('Ingresa tu E-mail'),
-];
 
-// multer
+
+// Validacion de Login
+
+/* const validateLoginForm = [
+    body('email').isEmail().notEmpty().withMessage('Ingresa tu E-meil'),
+    body('password').notEmpty().withMessage('Ingresar contrasena'),
+]; */
+
+
+
+// Multer
+
 const storage = multer.diskStorage({
 
     // donde guardamos los archivos
@@ -45,30 +58,37 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 
-// Detalle del usuario
-router.get('/userProfile/:userId', usersController.user);
 
-/*Logeo del usuario*/
+// Preferencias de usuario
 
-router.get('/login', usersController.login);
+router.get('/userProfile/:userId', usersController.user) 
 
-router.post('/login', valLogin, usersController.login);
- 
+
+
+// Login
+
+router.get('/login', usersController.login)
 /*--> Esto es una sugerencia, de crear una página con el perfil del usuario común además de la de Admin 
 Es decir, según el 'id' del usuario logueado, va a mostrar lo que corresponda al Admin (crear y borrar productos) o al Usuario (perfil del usuario) */
 
-//Crear un usuario
+
+
+// Register
+
 router.get('/register', usersController.register);
+router.post('/register', upload.single('imgProfile'), registerValidations, usersController.processToCreate); 
+//---> El orden de los parámetros es importante, porque si no las validaciones de error no se procesan correctamente. Primero se pasa el Multer y después las Validaciones
 
-router.post('/register', upload.single('imgProfile'), validacionesRegistro, usersController.processToCreate); //---> El orden de los parámetros es importante, porque si no las validaciones de error no se procesan correctamente. Primero se pasa el Multer y después las Validaciones
 
-// Editar un usuario
-/* router.get('/editUser/:id', userssController...;) --> A completar
-router.put('/editUser/:id', "(Acá subir imagen)", usersController...); --> A completar */
 
-// Eliminar un usuario 
+// Editar Preferencias
+
+router.get('/userProfile/:id/preference', usersController.preference) 
+router.put('/editUser/:id/preference', upload.single("imgProfile"), usersController.editProferences);
+
+
+
+// Eliminar usuario 
 /* router.delete('/deleteUser/:id', usersController...); --> A completar */
-
-
 
 module.exports = router;
