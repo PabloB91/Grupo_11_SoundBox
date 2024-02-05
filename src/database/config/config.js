20e82b968fd05dd402fcc3dfad51f83e6b4c7c01
FranCapi2005 <ftcapitani@gmail.com>
2024-02-04 21:00:07 -0300

    puse nombre en el config del sequelize(me habia olvidado xd)

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "database_development",
+    "database": "SoundBox_Grupo:11",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
