a5c11f9e382537ceadeaff721da9bbc172ec205f
Miguel vargas Navarro <mvargasnavarro19@gmail.com>
2024-02-09 18:37:34 -0500

    puerto

--- src/app.js
+++ src/app.js
@@ -72,7 +72,7 @@ app.use((req, res, next) => {
 /************* LLAMANDO AL SERVIDOR *************/
 const port = process.env.PORT || 3050;
 
-app.listen(3050, () => {
+app.listen(`${port}`, () => {
     console.log(`Servidor funcionando en: http://localhost:${port}`)
 });
 //*****************************************************************************************************\\
