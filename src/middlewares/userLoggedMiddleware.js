2e692dd3c8de93641979edc69c2009fdb38d838e
BautistaGarcia <bautista.garcia.oliver@gmail.com>
2024-02-22 18:38:12 -0300

    ahora si funciona el middleware de UserLogged que muestra o no parte del header

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -1,20 +1,8 @@
 function userLoggedMiddleware(req, res, next) {
-        res.locals.userIsLogged = false
-        res.locals.userIsAdmin = false
-     
-        const cookieUser = req.cookies.userCookie 
-         if (req.session.admin) {
-             res.locals.userIsAdmin = true
-             res.locals.admin = req.session.admin
-         }
-     
-        if(cookieUser){
-            req.session.userLogged = req.cookies.userCookie
-        }
-     
-        if (req.session.userLogged) {
-         res.locals.userIsLogged = true
-         res.locals.userLogged = req.session.userLogged
+        res.locals.userIsLogged = false //----> esta variable se en el header.ejs es la que define dentro del if si el usuario esta logueado o no, de momento es false.
+
+        if (req.session.userLoggedIn) {
+         res.locals.userIsLogged = true //----> y aca la misma variable cambia a true ya que hay un usuario logueado dontro del sessions. entonces en el if del header.ejs mostramos parte del header o no, segun el valor de la misma.
         }
         next()
      
