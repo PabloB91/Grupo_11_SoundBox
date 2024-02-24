366897f040a5677faa6f78389cc10df7e3c8131e
BautistaGarcia <bautista.garcia.oliver@gmail.com>
2024-02-23 22:27:05 -0300

    hice las diferentes facetas del header para cuando el usuario esta logueado o no

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -3,7 +3,15 @@ function userLoggedMiddleware(req, res, next) {
 
         if (req.session.userLoggedIn) {
          res.locals.userIsLogged = true //----> y aca la misma variable cambia a true ya que hay un usuario logueado dontro del sessions. entonces en el if del header.ejs mostramos parte del header o no, segun el valor de la misma.
+         
         }
+        
+        // if (userLoggedIn.user_type_id == 2) {
+        //         req.session.admin = userLoggedIn.dataValues;
+        //         console.log(req.session.admin, 'este usuario es admin');
+        //     } else {
+        //         console.log(req.session.admin, 'este usuario es comun');
+        //     }  
         next()
      
 }
